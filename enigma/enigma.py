bef8210f682d27e3713ae53c85df3e62071c1eda
Charles Pascoe <cpascoe95@gmail.com>
2015-04-01 21:18:04 +0200

    Added better displacement method (Rotor.rotate), plugboard functionality, EnigmaMachine.encode

--- enigma/enigma.py
+++ enigma/enigma.py
@@ -2,13 +2,17 @@ import string
 alph = string.ascii_uppercase
 
 
+class InvalidLetterException(Exception):
+    pass
+
+
 class Rotor:
     def __init__(self, setup_string, rollover, disp=0):
         self.mapping = [alph.index(letter) for letter in setup_string]
         self.next_rotor = None
         self.prev_roto = None
         self.disp = disp
-        self.rollover = alph.index(rollover)
+        self.rollover = alph.index(rollover) if rollover is not None else None
 
     def increment(self):
         if self.disp == self.rollover:
@@ -16,16 +20,18 @@ class Rotor:
         self.disp = (self.disp + 1) % len(alph)
 
     def encode(self, val):
-        self.next_rotor.encode((self.mapping[(val + self.disp) % len(alph)] + self.disp) % len(alph))
+        self.next_rotor.encode(self.rotate(self.mapping[self.rotate(val)]))
 
     def reflect(self, val):
-        # TODO: needs to use displacement
-        self.prev_rotor.reflect(self.mapping.index(val))
+        self.prev_rotor.reflect(self.rotate(self.mapping.index(self.rotate(val))))
+
+    def rotate(self, val):
+        return (val + self.disp) % len(alph)
 
 
 class Reflector(Rotor):
     def __init__(self, setup_string):
-        super().__init__(setup_string, setup_string[0], 0)
+        super().__init__(setup_string, None)
 
     def increment(self):
         pass
@@ -44,18 +50,38 @@ class Plugboard:
             mapping[b] = a
 
     def encode(self, val):
-        pass
+        return self.mapping[val]
 
 
 class EnigmaMachine:
-    def __init__(self, *rotors):
+    def __init__(self, rotors, reflector, plugboard):
         self.next_rotor = None
         prev_rotor = self
+
         for rotor in rotors:
             prev_rotor.next_rotor = rotor
             rotor.prev_rotor = prev_rotor
             prev_rotor = rotor
 
+        prev_rotor.next_rotor = reflector
+        reflector.prev_rotor = prev_rotor
+
+        self.plugboard = plugboard
+
+        self.output = None
+
+    def encode(self, letter):
+        if letter not in alph:
+            raise InvalidLetterException()
+
+        val = alph.index(letter)
+
+        self.next_rotor.encode(self.plugboard.encode(val))
+
+
     def reflect(self, val):
-        pass
+        result = self.plugboard.encode(val)
+
+        if self.output:
+            self.output(alph[result])
 
