6515d5054c730261d4602aa4d1950ee9f25cb6b5
Geraint White <geraintwhite@gmail.com>
2015-04-01 13:14:30 +0100

    Add reflector to tests

--- enigma/testing.py
+++ enigma/testing.py
@@ -3,4 +3,4 @@ import enigma
 rotor_1 = enigma.Rotor('EKMFLGDQVZNTOWYHXUSPAIBRCJ', 'Q')
 rotor_2 = enigma.Rotor('AJDKSIRUXBLHWTMCQGZNPYFVOE', 'E')
 rotor_3 = enigma.Rotor('BDFHJLCPRTXVZNYEIWGAKMUSQO', 'V')
-reflector = enigma.Rotor('QYHOGNECVPUZTFDJAXWMKISRBL')
+reflector = enigma.Reflector('QYHOGNECVPUZTFDJAXWMKISRBL')
