d816f085746c3360211c7f838e2745619491488d
Charles Pascoe <cpascoe95@gmail.com>
2015-04-01 14:09:17 +0200

    Started

--- enigma/enigma.py
+++ enigma/enigma.py
@@ -1,1 +1,12 @@
-class
+import string
+alph = string.ascii_uppercase
+
+class Rotor:
+    def __init__(self, setup_string, rollover):
+        self.mapping = [alph.index(letter) for letter in setup_String]
+        self.next_rotor = None
+        self.prev_roto = None
+        self.disp = 0
+        self.rollover = alph.index(rollover)
+        
+        
