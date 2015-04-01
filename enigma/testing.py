c77872f3399c97ca7a432af003f4990be4489795
Geraint White <geraintwhite@gmail.com>
2015-04-01 13:13:10 +0100

    Added rotors to tests

---
+++ enigma/testing.py
@@ -0,0 +1,6 @@
+import enigma
+
+rotor_1 = enigma.Rotor('EKMFLGDQVZNTOWYHXUSPAIBRCJ', 'Q')
+rotor_2 = enigma.Rotor('AJDKSIRUXBLHWTMCQGZNPYFVOE', 'E')
+rotor_3 = enigma.Rotor('BDFHJLCPRTXVZNYEIWGAKMUSQO', 'V')
+reflector = enigma.Rotor('QYHOGNECVPUZTFDJAXWMKISRBL')
