61b03ddea6d457466228deadf4313455db434cf6
Charles Pascoe <cpascoe95@gmail.com>
2015-04-01 14:22:06 +0200

    More functionality

--- enigma/enigma.py
+++ enigma/enigma.py
@@ -2,21 +2,46 @@ import string
 alph = string.ascii_uppercase
 
 class Rotor:
-    def __init__(self, setup_string, rollover):
+    def __init__(self, setup_string, rollover, disp=0):
         self.mapping = [alph.index(letter) for letter in setup_String]
         self.next_rotor = None
         self.prev_roto = None
-        self.disp = 0
+        self.disp = disp
         self.rollover = alph.index(rollover)
         
     def increment(self):
-        self.disp
+        self.disp = (self.disp + 1) % len(alph)
+        # Check rollover
     
     def encode(self, val):
-        self.next_rotor(self.mapping[val])
+        self.next_rotor.encode(self.mapping[val])
         
     def reflect(self, val):
-        self.prev_rotor(self.mapping.index(val))
+        self.prev_rotor.reflect(self.mapping.index(val))
         
 
+class Reflector(Rotor):
+    def increment(self):
+        pass
+    
+    def encode(self, val):
+        self.prev_rotor(self.mapping[val])
 
+
+class Plugboard:
+    pass
+
+
+class EnigmaMachine:
+    def __init__(self, *rotors):
+        self.next_rotor = None
+        prev_rotor = self
+        for rotor in rotors:
+            prev_rotor.next_rotor = rotor
+            rotor.prev_rotor = prev_rotor
+            prev_rotor = rotor
+    
+    def reflect(self, val):
+        
+        
+    
