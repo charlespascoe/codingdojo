caa5ba0eee575a4af623d0cdd633dd7c050c2cfd
Geraint White <geraintwhite@gmail.com>
2015-06-04 09:30:40 +0100

    Make data munging kata more generic

--- data_munging/main.py
+++ data_munging/main.py
@@ -1,13 +1,8 @@
 import re
 
 
-def min_temp(data):
-  m = [m for m in [re.findall('(?:\s+(\d+))', line) for line in data] if len(m) > 1]
-  x = min(m, key=lambda x: abs(int(x[1])-int(x[2])))
-  return int(x[0]), abs(int(x[1])-int(x[2]))
-
-def min_goal(data):
-  m = [m.groups() for m in [re.search('([A-Za-z_]+)(?:\s+\d+)+\s+(\d+)\s+-\s+(\d+)', line) for line in data] if m]
+def min_col(d, r):
+  m = [m.groups() for m in [re.search(r, l) for l in d] if m]
   x = min(m, key=lambda x: abs(int(x[1])-int(x[2])))
   return x[0], abs(int(x[1])-int(x[2]))
 
@@ -16,16 +11,20 @@ if __name__ == '__main__':
   with open('weather.dat') as f:
     weather = f.read().split('\n')
 
-  day, difference = min_temp(weather)
+  day, difference = min_col(weather, r'\s+(\d+)\s+(\d+)\s+(\d+)')
 
-  assert day == 14, 'incorrect day: {}'.format(day)
+  print('min temp difference: day {}, diff {}'.format(day, difference))
+
+  assert int(day) == 14, 'incorrect day: {}'.format(day)
   assert difference == 2, 'incorrect difference: {}'.format(difference)
 
 
   with open('football.dat') as f:
     football = f.read().split('\n')
 
-  team, difference = min_goal(football)
+  team, difference = min_col(football, r'([A-Za-z_]+)(?:\s+\d+)+\s+(\d+)\s+-\s+(\d+)')
+
+  print('min goal difference: team {}, diff {}'.format(team, difference))
 
   assert team == 'Aston_Villa', 'incorrect team: {}'.format(team)
   assert difference == 1, 'incorrect difference: {}'.format(difference)
