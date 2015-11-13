ee3072e3bec71dfb65710ebf6c2a69a878a5c4f8
Geraint White <geraintwhite@gmail.com>
2015-11-13 12:11:07 +0000

    Add more categories to Yahtzee scoring

--- yahtzee/lib/yahtzee.rb
+++ yahtzee/lib/yahtzee.rb
@@ -1,10 +1,30 @@
 class Yahtzee
   VALUES = {aces: 1, twos: 2, threes: 3, fours: 4, fives: 5, sixes: 6}
+
   def score (dice, category)
     if VALUES.has_key? category
       return dice.count(VALUES[category]) * VALUES[category]
     elsif category == :pair
-      return 8
+      return sum_of_run(dice, 2).max || 0
+    elsif category == :three_of_a_kind
+      return sum_of_run(dice, 3).max || 0
+    elsif category == :four_of_a_kind
+      return sum_of_run(dice, 4).max || 0
+    elsif category == :two_pairs
+      sums = sum_of_run(dice, 2)
+      return sums.length == 2 ? sums.inject(:+) : 0
+    elsif category == :small_straight
+      return dice.sort == [1,2,3,4,5] ? 15 : 0
+    elsif category == :large_straight
+      return dice.sort == [2,3,4,5,6] ? 20 : 0
+    elsif category == :full_house
+      pairs = sum_of_run(dice, 2)
+      three_of_a_kind = sum_of_run(dice, 3)
+      return pairs.length == 1 && three_of_a_kind.length == 1 ? dice.inject(:+) : 0
     end
+  end
+
+  def sum_of_run (dice, size)
+      dice.group_by{ |x| x }.values.select{ |x| x.length == size }.map{ |x| x.inject(:+) }
   end
 end
--- yahtzee/spec/yahtzee_spec.rb
+++ yahtzee/spec/yahtzee_spec.rb
@@ -19,12 +19,104 @@ describe Yahtzee do
 
     describe 'pair category' do
       tests = [
-        {dice: [3,3,3,4,4], score: 8}
+        {dice: [3,3,3,4,4], score: 8 },
+        {dice: [2,2,3,5,5], score: 10},
+        {dice: [2,1,3,6,5], score: 0 },
+        {dice: [2,2,2,6,5], score: 0 }
       ]
 
       tests.each do |test|
         it "should return #{test[:score]} for #{test[:dice]} in category pair" do
           expect(subject.score(test[:dice], :pair)).to eq test[:score]
+        end
+      end
+    end
+
+    describe 'two_pairs category' do
+      tests = [
+        {dice: [3,3,3,4,4], score: 0 },
+        {dice: [2,2,3,5,5], score: 14},
+        {dice: [2,1,3,6,5], score: 0 },
+        {dice: [2,2,2,6,5], score: 0 }
+      ]
+
+      tests.each do |test|
+        it "should return #{test[:score]} for #{test[:dice]} in category two_pairs" do
+          expect(subject.score(test[:dice], :two_pairs)).to eq test[:score]
+        end
+      end
+    end
+
+    describe 'three_of_a_kind category' do
+      tests = [
+        {dice: [1,1,1,2,3], score: 3},
+        {dice: [1,1,1,1,3], score: 0},
+        {dice: [1,1,3,2,3], score: 0}
+      ]
+
+      tests.each do |test|
+        it "should return #{test[:score]} for #{test[:dice]} in category three_of_a_kind" do
+          expect(subject.score(test[:dice], :three_of_a_kind)).to eq test[:score]
+        end
+      end
+    end
+
+    describe 'four_of_a_kind category' do
+      tests = [
+        {dice: [1,1,1,1,3], score: 4},
+        {dice: [2,2,2,2,3], score: 8},
+        {dice: [1,1,1,2,3], score: 0},
+        {dice: [5,5,5,5,5], score: 0}
+      ]
+
+      tests.each do |test|
+        it "should return #{test[:score]} for #{test[:dice]} in category four_of_a_kind" do
+          expect(subject.score(test[:dice], :four_of_a_kind)).to eq test[:score]
+        end
+      end
+    end
+
+    describe 'small_straight category' do
+      tests = [
+        {dice: [1,2,3,4,5], score: 15},
+        {dice: [5,3,1,2,4], score: 15},
+        {dice: [1,2,3,4,3], score: 0 },
+        {dice: [5,5,5,5,5], score: 0 }
+      ]
+
+      tests.each do |test|
+        it "should return #{test[:score]} for #{test[:dice]} in category small_straight" do
+          expect(subject.score(test[:dice], :small_straight)).to eq test[:score]
+        end
+      end
+    end
+
+    describe 'large_straight category' do
+      tests = [
+        {dice: [2,3,4,5,6], score: 20},
+        {dice: [5,3,6,2,4], score: 20},
+        {dice: [5,2,3,4,3], score: 0 },
+        {dice: [6,6,6,6,6], score: 0 }
+      ]
+
+      tests.each do |test|
+        it "should return #{test[:score]} for #{test[:dice]} in category large_straight" do
+          expect(subject.score(test[:dice], :large_straight)).to eq test[:score]
+        end
+      end
+    end
+
+    describe 'full_house category' do
+      tests = [
+        {dice: [2,2,4,4,4], score: 16},
+        {dice: [5,3,3,3,4], score: 0 },
+        {dice: [5,5,3,5,3], score: 21},
+        {dice: [6,6,6,6,6], score: 0 }
+      ]
+
+      tests.each do |test|
+        it "should return #{test[:score]} for #{test[:dice]} in category full_house" do
+          expect(subject.score(test[:dice], :full_house)).to eq test[:score]
         end
       end
     end
