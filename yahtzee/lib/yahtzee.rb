bd676be3a3eec13a49698eba379fb9a09ce0bac7
Geraint White <geraintwhite@gmail.com>
2015-11-13 12:22:11 +0000

    single-liners!

--- yahtzee/lib/yahtzee.rb
+++ yahtzee/lib/yahtzee.rb
@@ -6,21 +6,18 @@ class Yahtzee
       return dice.count(VALUES[category]) * VALUES[category]
     elsif category == :pair
       return sum_of_run(dice, 2).max || 0
+    elsif category == :two_pairs
+      return sum_of_run(dice, 2).length == 2 ? sum_of_run(dice, 2).inject(:+) : 0
     elsif category == :three_of_a_kind
       return sum_of_run(dice, 3).max || 0
     elsif category == :four_of_a_kind
       return sum_of_run(dice, 4).max || 0
-    elsif category == :two_pairs
-      sums = sum_of_run(dice, 2)
-      return sums.length == 2 ? sums.inject(:+) : 0
     elsif category == :small_straight
       return dice.sort == [1,2,3,4,5] ? 15 : 0
     elsif category == :large_straight
       return dice.sort == [2,3,4,5,6] ? 20 : 0
     elsif category == :full_house
-      pairs = sum_of_run(dice, 2)
-      three_of_a_kind = sum_of_run(dice, 3)
-      return pairs.length == 1 && three_of_a_kind.length == 1 ? dice.inject(:+) : 0
+      return sum_of_run(dice, 2).length == 1 && sum_of_run(dice, 3).length == 1 ? dice.inject(:+) : 0
     end
   end
 
